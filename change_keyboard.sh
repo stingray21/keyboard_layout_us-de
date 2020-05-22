b9e6b08eea497d7926adadbc740478482eda5ab4
Jochen <stingray21@gmx.de>
2020-05-22 15:40:56 +0200

    Update 2020-05-22

--- change_keyboard.sh
+++ change_keyboard.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-sudo cp "/usr/share/X11/xkb/symbols/us" "/usr/share/X11/xkb/symbols/us_backup-`date '+%Y%m%d'`"
+# sudo cp "/usr/share/X11/xkb/symbols/us" "/usr/share/X11/xkb/symbols/us_backup-`date '+%Y%m%d'`"
+# sudo cp "/usr/share/X11/xkb/symbols/us" "./us_backup-`date '+%Y%m%d'`"
 sudo cp "./us" "/usr/share/X11/xkb/symbols/us"
