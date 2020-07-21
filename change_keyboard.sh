bc42422627ee72021109905e7bc9e5ac2148c3ea
Jochen <stingray21@gmx.de>
2020-07-21 13:14:31 +0200

    update 2020-07-21

--- change_keyboard.sh
+++ change_keyboard.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-# sudo cp "/usr/share/X11/xkb/symbols/us" "/usr/share/X11/xkb/symbols/us_backup-`date '+%Y%m%d'`"
-# sudo cp "/usr/share/X11/xkb/symbols/us" "./us_backup-`date '+%Y%m%d'`"
-sudo cp "./us" "/usr/share/X11/xkb/symbols/us"
+sudo cp "/usr/share/X11/xkb/symbols/us" "/usr/share/X11/xkb/symbols/us_backup-`date '+%Y%m%d'`"
+sudo cp "/usr/share/X11/xkb/symbols/us" "./backups/us_backup-`date '+%Y%m%d'`"
+sudo cp "/usr/share/X11/xkb/symbols/us" "./us"
+
+read -p "Check changes and commit to git. Confirm with Y when ready. " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo cp "./us" "/usr/share/X11/xkb/symbols/us"
+fi
+
